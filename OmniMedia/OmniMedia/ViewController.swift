b141b12123de916ca21aa84d43ab2064fac90de6
Javier Benavides <javierbenavides@Javiers-Mac-mini.local>
2018-01-28 01:16:02 -0700

    gps locator

--- OmniMedia/OmniMedia/ViewController.swift
+++ OmniMedia/OmniMedia/ViewController.swift
@@ -7,12 +7,69 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    //Where am I
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    let manager = CLLocationManager()
+    lazy var geocoder = CLGeocoder()
+    var lat: Double = 0.0
+    var lng: Double = 0.0
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        
+        //let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        //let gpsLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        
+        //let region:MKCoordinateRegion = MKCoordinateRegionMake(gpsLocation, span)
+        
+        //locationLabel.text = String(location.altitude)
+        lat = location.coordinate.latitude
+        lng = location.coordinate.longitude
+        
+    }
+    
+    private func processResponse(withPlacemarks placemarks: [CLPlacemark]?, error: Error?) {
+        // Update View
+        
+        
+        if let error = error {
+            print("Unable to Reverse Geocode Location (\(error))")
+            locationLabel.text = "Unable to Find Address for Location"
+            
+        } else {
+            if let placemarks = placemarks, let placemark = placemarks.first {
+                locationLabel.text = placemark.locality
+            } else {
+                locationLabel.text = "No Matching Addresses Found"
+            }
+        }
+    }
+    
+    @IBAction func setCurrentLocationLabelText(_ sender: UIButton) {
+        let location = CLLocation(latitude: lat, longitude: lng)
+        
+        geocoder.reverseGeocodeLocation(location, completionHandler: {
+          (placemarks, error) in
+            self.processResponse(withPlacemarks: placemarks, error: error)
+            
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
     }
 
     override func didReceiveMemoryWarning() {
